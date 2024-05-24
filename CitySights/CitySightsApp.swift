658f572927bfaedb6cf9406ca786a9efbcadd5f0
Jev IRIE PIXEL <jev@iriepixel.com>
2024-05-24 11:37:55 +0100

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Jev Smolnikov on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
